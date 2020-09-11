b32c16dc98431597fb0b9a2df7586938c3f8d3c2
li jian <lijiancc0724@gmail.com>
2020-09-11 12:40:32 +0800

    upload project

---
+++ MobileAssigenment2/MobileAssigenment2.Android/SQL_Android.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MobileAssigenment2;
+using SQLite;
+
+[assembly: Xamarin.Forms.Dependency(typeof(MobileAssigenment2.Droid.SQL_Android))]
+namespace MobileAssigenment2.Droid
+{
+    public class SQL_Android : ISQLite
+    {
+        public SQLiteConnection GetConnection(string dbName)
+        {
+
+            var dbPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var path = System.IO.Path.Combine(dbPath, dbName);
+            var conn = new SQLiteConnection(path);
+            return conn;
+        }
+    }
+}
---
+++ MobileAssigenment2/MobileAssigenment2.Android/SplashActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Threading.Tasks;
+
+namespace MobileAssigenment2.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupwork = new Task(() => { SimulateStartup(); });
+            startupwork.Start();
+        }
+
+        async void SimulateStartup()
+        {
+            await Task.Delay(100);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
